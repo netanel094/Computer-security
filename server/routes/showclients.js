7d3b67220e34b8154eaa28be5a68d0900c85d41d
Shon Khundiashvili <shonk2603@gmail.com>
2023-05-03 14:31:50 +0300

    comments adding

--- server/routes/showclients.js
+++ server/routes/showclients.js
@@ -4,6 +4,7 @@ const con = require('../models/connection_create');
 const allQueries = require('../models/queries');
 
 router.get('/', async function (req, res) {
+  //This route is to return all the clients that exist in the database
   const allClients = await allQueries.getAllClients(con);
 
   if (allClients) return res.status(200).send(allClients);
