e9e6577d6398c0194f4bddb208256a6c67c89dd0
Shon Khundiashvili <shonk2603@gmail.com>
2023-05-06 15:29:03 +0300

    searching by sub string

--- server/routes/searchtable.js
+++ server/routes/searchtable.js
@@ -2,18 +2,15 @@ const express = require('express');
 const router = express.Router();
 const allQueries = require('../models/queries');
 const con = require('../models/connection_create');
+const validator = require('../security/securityFunctions');
 
 router.post('/', async function (req, res) {
-  const { first_name, last_name, city, phone_number } = req.body;
+  const { search_string } = req.body;
+
+  const newSearchString = validator.inputValidate(search_string);
 
   //Searching the client by one one the substrings (email, phone number, first name, last name, city)
-  const result = await allQueries.searchClient(
-    first_name,
-    last_name,
-    city,
-    phone_number,
-    con
-  );
+  const result = await allQueries.searchClient(newSearchString, con);
 
   if (result === false)
     return res.status(404).send('Did not find clients with this sub string');
